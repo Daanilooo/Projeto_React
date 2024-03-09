a4a7f4a03c6402450588a25bc4a40293fef6c83e
Danilo Souza <daanilosouza273@gmail.com>
2024-03-09 17:17:05 -0300

    Implementando styled-components no código

--- src/componentes/header/index.js
+++ src/componentes/header/index.js
@@ -1,16 +1,22 @@
-import './estilo.css'
 import Logo from '../logo'
 import OpcoesHeader from '../opcoesHeader'
 import IconesHeader from '../iconesHeader'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header `
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+
+`
 
 function header() {
     return (
-        <header className="App-header">
+        <HeaderContainer>
             <Logo />
             <OpcoesHeader />
             <IconesHeader />
-
-        </header>
+        </HeaderContainer>
     )
 }
 
